21e10b1dc6918df69fec8c9a252249f397af781c
LisaLudique <lisacarolynlu@gmail.com>
2016-09-18 08:43:23 -0400

    Added descriptive header

--- dronekit-python/examples/drone_delivery/drone_delivery.py
+++ dronekit-python/examples/drone_delivery/drone_delivery.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 """
-© Copyright 2015-2016, 3D Robotics.
+Flight Aid
+Built by: Lisa Lu for HackMIT 2016
+Based off of DroneKit's baseline drone delivery example, this web app allows for
+order placement and gives an estimated time of arrival for packages for users.
+--------------------------------------------------------------------------------
+© Drone Delivery Example Copyright 2015-2016, 3D Robotics.
 drone_delivery.py:
 
 A CherryPy based web application that displays a mapbox map to let you view the current vehicle position and send the vehicle commands to fly to a particular latitude and longitude.
